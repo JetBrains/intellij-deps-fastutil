d93d9d5de3c924cb7df174dec238df4433385950
develar <develar@gmail.com>
2020-05-04 17:09:17 +0200

    simply use out as result dir

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -ex
 
-curl -L https://repo1.maven.org/maven2/it/unimi/dsi/fastutil/8.3.1/fastutil-8.3.1.jar -o fastutil
-./find-deps.sh minimize ~/.m2/repository/it/unimi/dsi/fastutil/8.3.1/fastutil-8.3.1.jar dependencies.txt
+curl -L https://repo1.maven.org/maven2/it/unimi/dsi/fastutil/8.3.1/fastutil-8.3.1.jar -o "out/fastutil.jar"
+./find-deps.sh minimize "out/fastutil.jar" dependencies.txt
