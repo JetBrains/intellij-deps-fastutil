9a76f9fab4f8544be2998370d290c1d56ca5a5f9
develar <develar@gmail.com>
2020-05-05 19:20:48 +0200

    use maven assembly plugin

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-
-public class Main {
-  public Main() {
-    Object2IntMap<Object> objectObject2IntMap = new Object2IntOpenHashMap<>();
-  }
-}
